e769e000ef31e9ae533c3bed8c9475a6099f5e93
Dina Sakæussen <disa5@student.eadania.dk>
2026-07-03 00:11:03 +0200

    Add project files.

---
+++ 02-07 oevelse camping/Models/Nearest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _02_07_øvelse_camping.Models
+{
+    //jeg har lavet accessor til public fordi jeg vil bruge klassen et andet sted, derfor er den public så den er tilgængelig
+    public class Nearest
+    {
+
+    }
+}
---
+++ 02-07 oevelse camping/Program.cs
@@ -0,0 +1,4 @@
+﻿using _02_07_øvelse_camping.Models;
+//Jeg vil lave en ny bruger / user, (klassen er kaldt Users).
+//Så jeg navngav den første bruger "Camper1" og fortalte programmet at det er en ny bruger: new Users
+Users Camper1 = new Users();
