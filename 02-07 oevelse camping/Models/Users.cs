a91bb13755743879edec13734f5b931335e90711
Dina Sakæussen <disa5@student.eadania.dk>
2026-07-03 00:40:43 +0200

    starter på programmet, klasser er oprettet, opsætning af klasserne

--- 02-07 oevelse camping/Models/Users.cs
+++ 02-07 oevelse camping/Models/Users.cs
@@ -7,6 +7,8 @@ namespace _02_07_øvelse_camping.Models
     //jeg har lavet accessor til public fordi jeg vil bruge klassen et andet sted, derfor er den public så den er tilgængelig
     public class Users
     {
-
+        //Jeg vælger at sige at når man opretter en User, så skal User indeholde; Navn mm.
+        //Jeg sætter get og set til public, så jeg kan bruge det i andre klasser.
+        public string Name { get; set; }
     }
 }
