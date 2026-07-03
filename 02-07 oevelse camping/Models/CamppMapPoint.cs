9eb90209f52de42f7bfbc81176a633441b7590a0
Dina Sakæussen <disa5@student.eadania.dk>
2026-07-03 15:10:04 +0200

    oprettet en gps klasse

---
+++ 02-07 oevelse camping/Models/CamppMapPoint.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _02_07_øvelse_camping.Models
+{
+    //Efter installation af Google Maps Location Services i NuGet: Laver jeg først klassen public 
+    public class CamppMapPoint
+    {
+        //Jeg vælger at i klassen CamppMapPoint skal man oprette følgende: (double er valgt pga. decimaler i koordinaterne)
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
--- 02-07 oevelse camping/Models/Campsites.cs
+++ 02-07 oevelse camping/Models/Campsites.cs
@@ -7,6 +7,16 @@ namespace _02_07_øvelse_camping.Models
     //jeg har lavet accessor til public fordi jeg vil bruge klassen et andet sted, derfor er den public så den er tilgængelig
     public class Campsites
     {
+        //Jeg vælger at i klassen Campsites skal man oprette følgende properties; CampSiteName mm.
+        public string CampSiteName { get; set; }
+        public string CampSiteAdress { get; set; }
+        public int CampSiteAdressNumber {  get; set; }
+        public string CampSiteCity { get; set; }
+        public int CampSiteZipCode { get; set; }
+        public string CampSiteRegion { get; set; }
+        public string CampSiteCountry { get; set; }
+    
+
 
     }
 }
