2330891e13ad9f3e7bc7c524c91d8e4862532375
Dina Sakæussen <disa5@student.eadania.dk>
2026-07-04 10:27:40 +0200

    arbejder videre med klassen MapPoint som er omdøbt fra CampMapPoint

---
+++ 02-07 oevelse camping/Models/MapPoint.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace _02_07_øvelse_camping.Models
+{
+    //Efter installation af Google Maps Location Services i NuGet: Laver jeg først klassen public 
+    public class MapPoint
+    {
+        //Jeg vælger at i klassen skal man oprette Længde og Breddegrader
+        //Datatypen double er valgt pga. decimaler i koordinaterne
+        //Required attributten sikrer, at Latitude og Longitude skal udfyldes,
+        //for at undgå situationer, hvor enten at lokationsdataene ikke er udfyldt, eller hvor lokationsdataene er inkorrekt 
+        [Required]
+        public double Latitude { get; set; }
+        [Required]
+        public double Longitude { get; set; }
+    }
+}
